1ab6de38f1b1fe33b828369221bafd3069f738ad
Jerren Dang <djerren@gmail.com>
2024-06-22 16:16:45 +0000

    Project started

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,9 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.Account;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +11,21 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    // post /register
+// - The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a JSON of the Account, including its accountId. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+// - If the registration is not successful due to a duplicate username, the response status should be 409. (Conflict)
+// - If the registration is not successful for some other reason, the response status should be 400. (Client error)
+    @PostMapping("/register")
+    public Account register(@RequestBody Account account){
+        return account;
+    }
 
+
+    // post /login
+    // post /messages
+    // get /messages
+    // get /messages/{messageId}
+    // 
 }
--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,15 @@
 package com.example.repository;
 
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+import com.example.entity.Account;
+
+@Repository
 public interface AccountRepository {
+    private List<Account> accountList = new ArrayList<>();
+
+    public List<Account> getAll(){
+        
+    }
 }
